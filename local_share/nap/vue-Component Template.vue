71fd928e8a84202984d450032d576c41ecfa4b1d
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-02-04 17:44:31 +0800

    vue component!

---
+++ local_share/nap/vue-Component Template.vue
@@ -0,0 +1,16 @@
+<!-- 
+	https://vuejs.org/api/sfc-spec.html
+	Docs for Vue single file components
+-->  
+
+<script setup lang="ts">
+	// This script (tagged setup) runs once per component creation
+</script>
+
+<template>
+	<!-- Your HTML template here! -->
+</template>
+
+<style>
+	/* CSS Styling here! */
+</style>
