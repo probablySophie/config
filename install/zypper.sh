dbcea693f32b72cdacec1e1e10cc8e6ead52b40e
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-04-27 14:58:35 +0800

    aaaa

---
+++ install/zypper.sh
@@ -0,0 +1,15 @@
+# Install stuff that we use & need from zypper
+# For SUSE & OpenSUSE linux distros
+
+if command -v zypper &> /dev/null ; then
+	CMD=(
+		sudo zypper in -y
+		fzf # TUI Fuzzy finder & selector
+		tmux # TMUX!
+		make gcc cmake # friends for installing & building software
+		libnotify-tools # For commandline notifications
+	)
+	"${CMD[@]}"
+fi
+
+notify-send "Finished Installing!" -a "zypper.sh"
