82b073f37266b675c8629822df8a9d2ab791def0
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-05-06 18:20:14 +0800

    install stufffff

---
+++ install/cargo.sh
@@ -0,0 +1,15 @@
+rustup update
+
+cargo install starship --locked # starship makes our bash prompt prettier
+
+cargo install bottom --locked # Top replacement
+
+cargo install tuifeed --locked # TUI Rss feeds (probs going to replace)
+cargo install cargo-expand --locked # More cargo bits
+cargo install eza # An ls replacer (that also replaces exa)
+cargo install bat --locked # A cat replacer
+
+# The Yazi TUI file manager & fd/ripgrep for file searching
+cargo install --locked yazi-fm yazi-cli
+cargo install fd-find
+cargo install ripgrep
