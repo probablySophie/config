8abe120bd9ce3951750e0aa3adb4362b90d0d17b
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-05-26 05:36:47 +0000

    invisible files should now link

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,17 +5,37 @@ normal=$(tput sgr0)
 
 # Files link destination
 
+function symlink
+{
+	local file=$1
+	local link_to=$2
+	# Make sure the file we're linking from actually exists
+	if [[ -f $file ]] || [[ -d $file ]]; then
+		printf "\t${file##*/}\n";
+		ln -sf $file $link_to;
+	fi
+}
+
 function bulk_symlink
 {
 	printf "\nInput:  $1\n";
-	printf "Output: $2\n";
-	files=($1);
-	for file in "${files[@]}"; do
-		file_name="${file##*/}";
-		printf "\t$file_name\n";
-		ln -sf $file $2;
+	printf "Output into: $2\n";
+	for file in $1/*; do
+		symlink $file $2
+	done
+	for file in $1/.*; do
+		symlink $file $2
 	done
 }
 
-bulk_symlink "$PWD/config/*" "$HOME/.config/"
-bulk_symlink "$PWD/local_share/*" "$HOME/.local/share/"
+bulk_symlink "$PWD/config" "$HOME/.config/"
+bulk_symlink "$PWD/local_share" "$HOME/.local/share/"
+
+# Update ~/.bashrc to source our custom files if it doesn't already
+if [[ "$(grep -c 'include custom bash setup if it exists' ~/.bashrc)" == "0" ]]; then
+	printf "# include custom bash setup if it exists\n"\
+"if [ -f \$HOME/.config/bash/custom.sh ]; then\n"\
+"    . \$HOME/.config/bash/custom.sh\n"\
+"fi\n" >> ~/.bashrc
+	printf "Updated ~/.bashrc\n";
+fi
