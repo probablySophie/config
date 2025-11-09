2daaec1837d14d397ebd8801d894b3e5e326c8bb
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-11-09 19:55:37 +0800

    cargooo

--- install/cargo.sh
+++ install/cargo.sh
@@ -1,4 +1,5 @@
 rustup update
+rustup component add rust-analyzer
 
 cargo install starship --locked # starship makes our bash prompt prettier
 
@@ -13,6 +14,17 @@ cargo install bat --locked # A cat replacer
 cargo install --locked yazi-fm yazi-cli
 cargo install fd-find
 cargo install ripgrep
+
+cargo install --git https://github.com/latex-lsp/texlab --locked
+
+# Adding aliases
+mkdir -p ~/.local/completions
+
+bat --completion bash > ~/.local/completions/bat.sh
+echo "source ~/.local/completions/bat.sh" >> ~/.bashrc
+
+rg --generate complete-bash > ~/.local/completions/ripgrep.sh
+echo "source ~/.local/completions/ripgrep.sh" >> ~/.bashrc
 
 if command -v notify-send &> /dev/null ; then
 	notify-send "Finished Installing!" -a "cargo.sh"
