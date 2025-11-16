f04251df06d6847e264ecf7b23bf064514393600
Sophie <sophie@localhost.localdomain>
2025-11-16 19:53:53 +0800

    more bits!

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -1,8 +1,7 @@
-flatpak install flathub \
-	
+
 if command -v flatpak &> /dev/null ; then
 	CMD=(
-		zypper install
+		flatpak install -y flathub
 		com.github.tchx84.Flatseal # Flatseal for flatpak permissions management
 		org.darktable.Darktable # Image management
 		org.gnome.Boxes # Boxes for VM management
@@ -11,8 +10,9 @@ if command -v flatpak &> /dev/null ; then
 		org.libreoffice.LibreOffice # LibreOffice as an office suite
 		org.mozilla.Thunderbird # Thunderbird for emails
 		org.mozilla.firefox # Firefox for internet
+		org.gimp.GIMP # Image editing :)
 	)
 	"${CMD[@]}"
 fi
 
-notify-send "Finished Installing!" -a "zypper.sh"
+notify-send "Finished Installing!" -a "flatpak.sh"
