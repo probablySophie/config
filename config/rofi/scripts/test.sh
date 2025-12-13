c03fa54ae07bf6f2a71d3dde201e2253fb2da19d
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2025-12-13 12:56:22 +0800

    AAAAAAAAAA

--- config/rofi/scripts/test.sh
+++ config/rofi/scripts/test.sh
@@ -3,21 +3,54 @@
 # Examples
 # https://github.com/davatorium/rofi-scripts
 
+# Arch Linux Man Page
+# https://man.archlinux.org/man/rofi-script.5.en
+
 ROFI_CONFIG_PATH="$HOME/.config/rofi";
 
 function rofi_scripts()
 {
 	ls "${ROFI_CONFIG_PATH}/scripts"
 }
+function run_rofi_scripts_selection()
+{
+	if [[ "$1" == "" ]]; then
+		if [[ "$2" != true ]]; then
+			printf "No selection\n";
+		fi
+		return
+	fi
+	# There was a selection - run it
+	if [[ "$2" != true ]]; then
+		printf "You selected $1\n";
+		. "${ROFI_CONFIG_PATH}/scripts/$1";
+	else
+		# notify-send "Running ${ROFI_CONFIG_PATH}/scripts/$1";
+		ROFI_RETV=""; # Clear RETV so the script runs as new
+		bash -e "${ROFI_CONFIG_PATH}/scripts/$1";
+	fi
+}
+function run_rofi_scripts()
+{
+	local SCRIPT_SELECTION=$( rofi_scripts | rofi -dmenu run -p "Select a script" -theme "${ROFI_CONFIG_PATH}/styles/test.rasi" );
+	run_rofi_scripts_selection "$SCRIPT_SELECTION";
+}
 # Pipe all scripts in the rofi script file folder into rofi
-SCRIPT_SELECTION=$( rofi_scripts | rofi -dmenu run -p "Select a script" -theme "${ROFI_CONFIG_PATH}/styles/test.rasi" )
 
-# No selection?  Exit
-if [[ "${SCRIPT_SELECTION}" == "" ]]; then
-	printf "No selection\n";
+if [[ "$ROFI_RETV" == "" ]]; then
+	run_rofi_scripts;
 	return
 fi
-# There was a selection - run it
-printf "You selected ${SCRIPT_SELECTION}\n";
-. "${ROFI_CONFIG_PATH}/scripts/${SCRIPT_SELECTION}";
+
+# We've been asked what our options are
+if [[ "$ROFI_RETV" == 0 ]]; then
+	rofi_scripts
+	exit
+fi
+# Something was selected! ($@ contains the selection)
+if [[ "$ROFI_RETV" == 1 ]]; then
+	coproc ( run_rofi_scripts_selection "$@" true > /dev/null 2>&1 ) &
+fi
+
+# No selection?  Exit
 
