b1fbbc430df1af8fb12a3195ee43f63f09adb2d0
Sophie <sophie@localhost.localdomain>
2026-01-12 17:08:36 +0800

    more bits & better comments
    comment

--- install/sway.sh
+++ install/sway.sh
@@ -1,14 +1,18 @@
+# Things wanted/needed for sway
 
 if command -v zypper &> /dev/null ; then
 	CMD=(
 		sudo zypper in -y
-			pamixer # Audio stufff
-			playerctl # MORREEE AUDIO STUFFFF
 			pavucontrol # Audio controls
+			pamixer # Audio level controls for Pulse Audio
+			playerctl # Command-line utility for controlling (multiple) music players
+			mpd # Music player daemon
 			SwayNotificationCenter # Notifications!
 			brightnessctl # For...
 	)
 	"${CMD[@]}"
 fi
+
+# TODO: Do we actually need both pavucontrol and pamixer?  They appear to both do the same thing.
 
 notify-send "Finished Installing!" -a "sway.sh"
