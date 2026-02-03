9ea05b7e132792eff781a6a6b746f441c96825e4
Sophie <sophie@localhost.localdomain>
2026-02-03 18:27:38 +0800

    shhhhh

--- install/language_server_protocols.sh
+++ install/language_server_protocols.sh
@@ -6,10 +6,10 @@ GLOBAL_INSTALLS=(
 	typescript
 	typescript-language-server
 	vscode-langservers-extracted 
-	vscode-langservers-extracted/vscode-html-language-server 
-	vscode-langservers-extracted/vscode-css-language-server 
-	vscode-langservers-extracted/vscode-json-language-server 
-	vscode-langservers-extracted/vscode-eslint-language-server 
+	# vscode-langservers-extracted/vscode-html-language-server 
+	# vscode-langservers-extracted/vscode-css-language-server 
+	# vscode-langservers-extracted/vscode-json-language-server 
+	# vscode-langservers-extracted/vscode-eslint-language-server 
 	bash-language-server 
 	pyright 
 );
