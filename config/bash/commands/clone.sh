6b949db6204ea7f0190f006fc21e3df12df9afe0
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-02-13 14:53:48 +0800

    minor reorder

--- config/bash/commands/clone.sh
+++ config/bash/commands/clone.sh
@@ -4,15 +4,17 @@
 custom_command "clone" "List repos belonging to the current 'gh' signed in account and clone the selected one"
 function clone
 {
-	if [[ "$1" == "" ]]; then
-		gh repo list -L 100 \
-			| fzf \
-			| sed 's/[ \t].*//g' \
-			| xargs -r -I {} gh repo clone {} -- --recurse-submodules
-		return
-	fi
 	# Incase we forget to `git clone` and just tried to `clone`
-	git clone "$1"
+	if [[ "$1" != "" ]]; then
+		git clone "$1"
+		return		
+	fi
+	# Else
+
+	gh repo list -L 100 \
+		| fzf \
+		| sed 's/[ \t].*//g' \
+		| xargs -r -I {} gh repo clone {} -- --recurse-submodules
 }
 
 # TODO: Submodule command?
