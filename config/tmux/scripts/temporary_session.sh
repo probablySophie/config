b487a2f1934afd460fea7bfed26919cd23b9f64f
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-03-09 16:42:10 +0800

    workaround: session instead of popup
    Yazi has been crashing the entire session when opened in a tmux popup.
    No idea why, happens on latest versions, happens on multiple devices.

---
+++ config/tmux/scripts/temporary_session.sh
@@ -0,0 +1,8 @@
+# Using && so we know that the command has finished executing before the next one runs
+SESSION_NAME="$1";  if [[ "$SESSION_NAME" == "" ]]; then SESSION_NAME="temp_session"; fi
+RUN_COMMAND="$2";
+CURRENT_SESSION="$(tmux display-message -p "#S")";
+
+tmux new-session -c "$PWD" -d -s "$SESSION_NAME" \
+	&& tmux send-keys -t "$SESSION_NAME" "$RUN_COMMAND; tmux switch-client -t '$CURRENT_SESSION' && tmux kill-session" ENTER \
+	&& tmux switch-client -t "$SESSION_NAME";
