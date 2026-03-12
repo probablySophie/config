af39870b939c3ad079eef70d00e44413915d6de4
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-03-12 10:22:39 +0800

    fixes: now uses pane's dir, not root session dir

--- config/tmux/scripts/temporary_session.sh
+++ config/tmux/scripts/temporary_session.sh
@@ -3,6 +3,6 @@ SESSION_NAME="$1";  if [[ "$SESSION_NAME" == "" ]]; then SESSION_NAME="temp_sess
 RUN_COMMAND="$2";
 CURRENT_SESSION="$(tmux display-message -p "#S")";
 
-tmux new-session -c "$PWD" -d -s "$SESSION_NAME" \
+tmux new-session -c "$(tmux display-message -p "#{pane_current_path}")" -d -s "$SESSION_NAME" \
 	&& tmux send-keys -t "$SESSION_NAME" "$RUN_COMMAND; tmux switch-client -t '$CURRENT_SESSION' && tmux kill-session" ENTER \
 	&& tmux switch-client -t "$SESSION_NAME";
