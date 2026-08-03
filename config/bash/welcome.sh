41d5a528a6278a813bf0618a189e1c05f47d7e42
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-08-03 12:39:47 +0800

    ehhh?

--- config/bash/welcome.sh
+++ config/bash/welcome.sh
@@ -6,38 +6,68 @@ plural() {
 	fi
 }
 
-# TODO: We need to [[ -f /proc/uptime ]] and if it doesn't exist do something else
+UPTIME_STRING="";
+# Not all systems have /proc/uptime
+if [[ -f /proc/uptime ]]; then
+	uptime=$(cat /proc/uptime | cut -d ' ' -f 1) # get the uptime
+	uptime=${uptime%.*} # float -> int
 
-uptime=$(cat /proc/uptime | cut -d ' ' -f 1) # get the uptime
-uptime=${uptime%.*} # float -> int
+	uptime_days=$((uptime/86400))			# how many days
+	uptime=$((uptime%86400))				# remove the days
 
-uptime_days=$((uptime/86400))			# how many days
-uptime=$((uptime%86400))				# remove the days
+	uptime_hours=$((uptime/3600))			# how many hours
+	uptime=$((uptime%3600))					# remove the hours
 
-uptime_hours=$((uptime/3600))			# how many hours
-uptime=$((uptime%3600))					# remove the hours
+	uptime_minutes=$((uptime/60))
+	uptime_seconds=$((uptime%60))
 
-uptime_minutes=$((uptime/60))
-uptime_seconds=$((uptime%60))
+	UPTIME_STRING+="$(printf "Uptime: ${purple}${uptime_days}${normal} day"	)"
+	UPTIME_STRING+="$(plural $uptime_days)"
+	UPTIME_STRING+="$(printf " ${purple}${uptime_hours}${normal} hour"		)"
+	UPTIME_STRING+="$(plural $uptime_hours)"
+	UPTIME_STRING+="$(printf " ${purple}${uptime_minutes}${normal} minute"	)"
+	UPTIME_STRING+="$(plural $uptime_minutes)"
+	UPTIME_STRING+="$(printf " ${purple}${uptime_seconds}${normal} second"	)"
+	UPTIME_STRING+="$(plural $uptime_seconds)"
+fi
+# if command -v uptime &> /dev/null; then
+# 	UPTIME_STRING="$(uptime \
+# 		| awk '{
+# 		str=$0;
+# 		# Remove the current time & "up"
+# 		sub(/^\s*[0-9]+:[0-9]+:[0-9]+\s+up\s+/,"",str);
+# 		sub(/,\s*[0-9]+\s*user.*$/,"",str);
+# 		patsplit(str, a, /[0-9]+/);
 
-shell_name=$(ps | grep $(echo $$) | rev | cut -d ' ' -f 1 | rev)
+# 		if ( length(a) == 3 ) { days=a[1]; hours=a[2]; mins=a[3]; }
+# 		else { days=0; hours=a[1]; mins=a[2]; }
 
-printf "%b" "Shell ${purple}${shell_name}${normal} is running in ${purple}${TERM}${normal} (${purple}${HOSTTYPE}${normal} os)\n"
+# 		if ( days > 0 ) { printf days; printf " day"; if ( days > 1 ) { printf "s" } printf " " }
+
+# 		if ( hours > 0 ) { printf hours; printf " hour"; if ( hours > 1 ) { printf "s" } printf " " }
+	
+	
+# 		if ( mins > 0 ) { printf mins; printf " min" }
+# 		if ( mins > 1 ) { printf "s" }
+# 	}");
+# fi
+
+shell_name="";
+if [[ "$SHELL" == "" ]]; then
+	shell_name=$(ps | grep $(echo $$) | rev | cut -d ' ' -f 1 | rev);
+else
+	shell_name="$SHELL";
+fi
+
+printf "%b" "${purple}${shell_name}${normal} is running in ${purple}${TERM}${normal} (${purple}${HOSTTYPE}${normal} os)\n"
 
 printf " /\_/\  "
-	printf "Uptime: ${purple}${uptime_days}${normal} day"	
-	plural $uptime_days
-	printf " ${purple}${uptime_hours}${normal} hour"		
-	plural $uptime_hours
-	printf " ${purple}${uptime_minutes}${normal} minute"	
-	plural $uptime_minutes
-	printf " ${purple}${uptime_seconds}${normal} second"	
-	plural $uptime_seconds
+printf "$UPTIME_STRING"
 
 printf "\n( o.o ) It is ${purple}%(%F)T${normal}.${purple} %(%R)T${normal} local time\n" -1 -1
 
 printf " > ^ <  User: ${purple}${USER}${normal}@${purple}$(hostname)${normal}\n" 
-printf "IP: ${purple}$(hostname -I | cut -d' ' -f1)${normal}\n"
+# printf "IP: ${purple}$(hostname -I | cut -d' ' -f1)${normal}\n"
 
 printf " ${red}E"
 printf "${orange}n"
