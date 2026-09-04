e2b978b64ae64d51799f887114e82efbcff7990d
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2026-09-04 15:28:33 +0800

    installing things on windows

---
+++ install/windows.ps1
@@ -0,0 +1,6 @@
+# Doesn't require admin
+winget install --id Microsoft.VisualStudioCode --source winget
+
+# Does require admin
+winget install --id Mozilla.Firefox --source winget
+winget install --id Microsoft.PowerShell --source winget
